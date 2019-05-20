e8c61219962cce27bd5f64ff07490b311aee7966
sunlong <2013855675@qq.com>
2019-05-20 18:01:53 +0800

    测试脚本

--- demo1/src/main.go
+++ demo1/src/main.go
@@ -11,14 +11,10 @@ type ResponseData struct {
 	Hostname string `json:"hostname"`
 }
 
-func test(w http.ResponseWriter, r *http.Request) {
-	hostname, _ := os.Hostname()
-	resp := ResponseData{}
-	resp.Hostname = hostname
-	_, _ = fmt.Fprint(w, resp)
-}
-
 func main() {
-	http.HandleFunc("/api/test", test)
+	http.HandleFunc("/api/test", func(w http.ResponseWriter, r *http.Request) {
+		hostname, _ := os.Hostname()
+		_, _ = fmt.Fprint(w, ResponseData{Hostname: hostname})
+	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
